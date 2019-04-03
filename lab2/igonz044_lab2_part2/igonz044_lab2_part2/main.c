a2658c52c641be6e744688148b3e153982ef1b8a
Itzel <44009297+igonz044@users.noreply.github.com>
2019-04-03 11:47:19 -0700

    lab1

---
+++ lab2/igonz044_lab2_part2/igonz044_lab2_part2/main.c
@@ -0,0 +1,18 @@
+/*
+ * igonz044_lab2_part2.c
+ *
+ * Created: 4/3/2019 11:44:03 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
