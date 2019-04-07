a08cf6c3f00859bb030bb3cd9ad6405728494af3
Itzel <44009297+igonz044@users.noreply.github.com>
2019-04-07 11:51:19 -0700

    lab1 part2 done

---
+++ lab1/igonz044_lab1_part2/igonz044_lab1_part2/main.c
@@ -0,0 +1,36 @@
+/*
+ * igonz044_lab1_part2.c
+ *
+ * Created: 4/3/2019 11:02:33 AM
+ * Author : ucrcse
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+	//DDR - registers
+	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
+	DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
+	
+	unsigned char spaces = 4;
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		unsigned char cntavail = spaces;
+		unsigned char i = 0;
+		while(spaces > i)
+		{
+			if(PINA & (0x01 << i))
+			{
+				cntavail--;
+			}//end of if
+			i++;
+		}//end of while
+		PORTC = cntavail;
+    }
+	return 0; 
+}
+
