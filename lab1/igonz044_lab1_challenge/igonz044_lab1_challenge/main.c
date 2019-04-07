3c79b8f87f359e8b20b52c64100c2e70c1a27560
Itzel <44009297+igonz044@users.noreply.github.com>
2019-04-07 12:45:29 -0700

    challengekindadone

---
+++ lab1/igonz044_lab1_challenge/igonz044_lab1_challenge/main.c
@@ -0,0 +1,36 @@
+/*
+ * igonz044_lab1_challenge.c
+ *
+ * Created: 4/7/2019 12:25:50 PM
+ * Author : ucrcse
+ */ 
+#include <avr/io.h>
+
+int main(void)
+{
+    /*AMUSEMENT PARK RIDE*/
+	//weight sensors weight 0 - 255 
+	
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0x00; PORTB = 0xFF;
+	DDRC = 0x00; PORTC = 0xFF;
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	unsigned char seatA = PINA;
+	unsigned char seatB = PINB;
+	unsigned char seatC = PINC;
+	unsigned char weight_sensor = seatA + seatB + seatC;
+	
+    while (1) 
+    {
+		if(weight_sensor > 140)
+		{
+			PORTA = 0x01;
+		}
+		else if(seatA - seatC > 80)
+		{
+			PORTB = 0x01;
+		}
+    }
+}
+
