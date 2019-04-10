87e4107821ed37144afc533ffafe33f264971fe3
Itzel <44009297+igonz044@users.noreply.github.com>
2019-04-10 01:28:26 -0700

    switched nibbles around and stuff

---
+++ lab2/igonz044_lab2_part4/igonz044_lab2_part4/main.c
@@ -0,0 +1,54 @@
+/*
+ * igonz044_lab2_part4.c
+ *
+ * Created: 4/10/2019 1:07:12 AM
+ * Author : ucrcse
+ */ 
+/* 
+ * Name & Email: Itzel G. igonz044@ucr.edu
+ * Lab Section: 026
+ * Assignment: igonz044_lab2_part4.c
+ * Created: 4/10/2019 1:07:12 AM
+ * Author : ucrcse
+ * Exercise Description: Switching nibbles around
+ *
+ * I acknowledge all content contained herein, excluding template or example
+ * code, is my own original work.
+ */
+
+#include <avr/io.h>
+
+//Bit access functions
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b) {
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k) {
+	return ((x & (0x01 << k)) != 0);
+}
+unsigned char botNibl(unsigned char num){return (0x0F & num);}
+unsigned char topNibl(unsigned char num){return (0xF0 & num);}
+
+int main(void)
+{
+	DDRA = 0x00; PORTA = 0xFF;//input
+	DDRB = 0xFF; PORTB = 0x00;//output
+	DDRC = 0xFF; PORTC = 0x00;//output
+	
+	unsigned char tmpA = 0x00;
+	unsigned char tmpB= 0x00;
+	
+    /* Replace with your application code */
+    while (1) 
+    {
+		//Step 1: Shift input PINA 
+		tmpA = PINA>>4;
+		tmpB = PINA<<4;
+		//Step 2: Mask lower nibble of PortB
+		PORTB = topNibl(PORTB) + tmpA;
+		PORTC = tmpB + botNibl(PORTC);
+		
+		//Step 3: Add these together and set to PORTB ..Nevermind!
+		/*PORTB = tmpA + tmpB1; //If PINA */
+    }
+}
+
