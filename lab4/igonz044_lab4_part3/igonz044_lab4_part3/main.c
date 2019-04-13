a52abbca385c2f2cc85c28b04a51082183eb2fa4
Itzel <44009297+igonz044@users.noreply.github.com>
2019-04-12 23:34:54 -0700

    Update main.c

--- lab4/igonz044_lab4_part3/igonz044_lab4_part3/main.c
+++ lab4/igonz044_lab4_part3/igonz044_lab4_part3/main.c
@@ -1,118 +1,77 @@
- /**
- * Created: 4/19/2018 7:48:48 AM
- * Author : Tanish
+/*
+ * Name & Email: Itzel G. igonz044@ucr.edu
+ * Lab Section: 026
+ * Assignment: igonz044_lab4_part3.c 
+ * Created: 4/12/2019 10:36:24 AM
+ * Author : ucrcse
+ * Exercise Description: Security System
+ *
+ * I acknowledge all content contained herein, excluding template or example
+ * code, is my own original work.
  */ 
-
 #include <avr/io.h>
+enum States {Start, locked, entry1, wait, entry2, unlocked}state;
 
-enum LA_States { LA_SMStart, init, check, increment, decrement, wait, wait1, reset } LA_State;
-
+//Global variables here
 #define A0 (~PINA & 0x01)
 #define A1 (~PINA & 0x02)
+#define A2 (~PINA & 0x03)
 
-void TickFct_LED()
+unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b){
+	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
+}
+unsigned char GetBit(unsigned char x, unsigned char k){
+	return ((x & (0x01 << k)) != 0);
+}
+
+void tick()
 {
-	switch(LA_State) {   // Transitions
-		case LA_SMStart:  // Initial transition
-		LA_State = init;
-		break;
-
-		case init:
-		LA_State = check;
+	switch(state)//Transitions
+	{
+		case Start:
+			state = locked;
 		break;
 		
-		case check:
-		if(!A0 && !A1){
-			LA_State = check;
-			}else if(A0 && !A1){
-			LA_State = increment;
-			}else if(!A0 && A1){
-			LA_State = decrement;
-			}else if(A0 && A1){
-			LA_State = reset;
-		}
+		case locked:
+			state = wait;
 		break;
 		
-		case increment:
-		LA_State = wait;
-		break;
-		
-		case decrement:
-		LA_State = wait1;
+		case entry1:
+			if(A2){ state = wait;}
+			else  { state = locked;}
 		break;
 		
 		case wait:
-		if(A0 && A1){
-			LA_State = reset;
-			}else if(!A0 ){ //&& !A1
-			LA_State = check;
-			}else if (A0 ){//&& !A1
-			LA_State = wait;
-		}
+			if(A2) { state = wait;}
+			else   {state = wait;}
 		break;
 		
-		case wait1:
-		if(A0 && A1){
-			LA_State = reset;
-			}else if(!A1){
-			LA_State = check;
-			}else if(A1){
-			LA_State = wait1;
-		}
+		case entry2:
+			if(A1){ state = unlocked;}
+			else  { state = locked;}
 		break;
 		
-		case reset:
-		LA_State = check;
+		case unlocked://opens vault
 		break;
 
-		default:
-		LA_State = LA_SMStart;
-		break;
-	} // Transitions
-
-	switch(LA_State) {   // State actions
-		case init:
-		PORTB = 0x07;
-		break;
-
-		case check:
+		default: break;
+	}	
+	switch (state) { //State Actions
+		case Start:
+		//state = locked;
 		break;
 		
-		case increment:
-		if( PORTB < 9 ){
-			PORTB = PORTB + 1;
-		}
+		case locked:
 		break;
 		
-		case decrement:
-		if( PORTB > 0 ){
-			PORTB = PORTB - 1;
-		}
+		case entry1:
 		break;
 		
 		case wait:
 		break;
 		
-		case wait1:
+		case entry2:
 		break;
 		
-		case reset:
-		PORTB = 0x00;
-		break;
-
-		default:
-		break;
-	} // State actions
-}
-
-int main(void) {
-	DDRA=0x00; PORTA=0xFF;
-	DDRB=0xFF; PORTB=0x00;
-	PORTB = 0x07;              // Initialize outputs
-	
-	LA_State = LA_SMStart; // Indicates initial call
-
-	while(1) {
-		TickFct_LED();
-	}
-}
+		case unlocked:
+		PORTB = 0x01;
