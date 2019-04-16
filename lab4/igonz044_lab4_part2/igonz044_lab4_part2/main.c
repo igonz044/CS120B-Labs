88e4f641c185234176188849dd3ec84d14714f7b
Itzel <44009297+igonz044@users.noreply.github.com>
2019-04-16 16:48:52 -0700

    part 2 new and improved

--- lab4/igonz044_lab4_part2/igonz044_lab4_part2/main.c
+++ lab4/igonz044_lab4_part2/igonz044_lab4_part2/main.c
@@ -13,9 +13,12 @@
 //#include “sm.h”
 
 
-enum States {bla, Start, Init, Inc, Dec, Reset, Stay} state;
+enum States {Init, Inc, Dec, Reset, Stay, Stay2} state;
 
 //Global variables here
+#define I (PINA & 0x01)//xxxx 0001
+#define D (PINA & 0x02)//xxxx 0010
+//#define R (GetBit(PINA, 0) && GetBit(PINA, 1))//xxxx 0011
 
 unsigned char Increment(unsigned char x){ return x += 1;}
 unsigned char Decrement(unsigned char x){ return x -= 1;}
@@ -30,104 +33,146 @@ unsigned char GetBit(unsigned char x, unsigned char k){
 
 void tick()
 {
-	switch (state) { //Transitions
-		case bla:
-			state = Start;
-		break;
-		
-		case Start:
-			state = Init;
-		break;
-		
+ 	switch (state) { //Transitions
 		case Init:
 		//In this state you can dec, incr, or reset from here
-			if(GetBit(PINA, 0) && PORTB <= 0x09){//if incrementing
-    			state = Inc;//increment
+			if(GetBit(PINA, 0) && !GetBit(PINA, 1) && PORTB <= 0x09)
+			{
+				state = Inc;
 			}
-			else if (GetBit(PINA, 1) && PORTB >= 0x00){
-    			state = Dec; //decrement
+			else if(GetBit(PINA, 1) && !GetBit(PINA, 0) && PORTB >= 0x00)
+			{
+				state = Dec;
 			}
-			else if (GetBit(PINA, 0)&&GetBit(PINA, 1)){//both pressed together
-    			state = Reset;
+			else if(GetBit(PINA, 0) && GetBit(PINA, 1))
+			{
+				state = Reset;
 			}
-			else if(!GetBit(PINA, 0) && !GetBit(PINA, 1)){ state = Init; }
+			else
+			{
+				state = Init;
+			}
 		break;
         
         case Inc:
-            if(GetBit(PINA, 0) && PORTB <= 0x09){//if incrementing
-                state = Stay;//increment
-            }
-            else if (GetBit(PINA, 1) && PORTB >= 0x00){
-                state = Dec; //decrement
-            }
-            else if (GetBit(PINA, 0) && GetBit(PINA, 1)){//both pressed together
-                state = Reset;
-            }
-            else if(!GetBit(PINA, 0) && !GetBit(PINA, 1)){ state = Init; }
+			if(GetBit(PINA, 0) && !GetBit(PINA, 1) && PORTB <= 0x09)
+			{
+				state = Stay;
+			}
+			else if(GetBit(PINA, 1) && !GetBit(PINA, 0) && PORTB >= 0x00)
+			{
+				state = Dec;
+			}
+			else if(GetBit(PINA, 0) && GetBit(PINA, 1))
+			{
+				state = Reset;
+			}
+			else
+			{
+				state = Stay;
+			}
         break;
         
         case Dec:
-            if(GetBit(PINA, 0) && PORTB <= 0x09){//if incrementing
-                state = Inc;//increment
-            }
-            else if (GetBit(PINA, 1) && PORTB >= 0x00){
-                state = Stay; //decrement
-            }
-            else if (GetBit(PINA, 0) && GetBit(PINA, 1)){//both pressed together
-                state = Reset;
-            }
-            else if(!GetBit(PINA, 0) && !GetBit(PINA, 1)){ state = Init; }
+			if(GetBit(PINA, 0) && !GetBit(PINA, 1) && PORTB <= 0x09)
+			{
+				state = Inc;
+			}
+			else if(GetBit(PINA, 1) && !GetBit(PINA, 0) && PORTB >= 0x00)
+			{
+				state = Stay2;
+			}
+			else if(GetBit(PINA, 0) && GetBit(PINA, 1))
+			{
+				state = Reset;
+			}
+			else
+			{
+				state = Stay2;
+			}
         break;
         
-        case Stay://check if buttons have been released
-            //not pressed, count stays the same
-            if(GetBit(PINA, 0) && PORTB <= 0x09){//if incrementing
-                state = Stay; 
-            }
-            if (GetBit(PINA, 1) && PORTB >= 0x00){//decrement
-                state = Stay; 
-            }
-            if (GetBit(PINA, 0) && GetBit(PINA, 1)){//both pressed together
-                state = Reset;
-            }
-            else if(!GetBit(PINA, 0) && !GetBit(PINA, 1)){ state = Init; }
+        case Stay:
+			if(GetBit(PINA, 0) && !GetBit(PINA, 1) && PORTB <= 0x09)
+			{
+				state = Stay;
+			}
+			else if(GetBit(PINA, 1) && !GetBit(PINA, 0) && PORTB >= 0x00)
+			{
+				state = Dec;
+			}
+			else if(GetBit(PINA, 0) && GetBit(PINA, 1))
+			{
+				state = Reset;
+			}
+			else
+			{
+				state = Stay;
+			}
         break;
-        
+		
+		case Stay2:
+			if(GetBit(PINA, 0) && !GetBit(PINA, 1) && PORTB <= 0x09)
+			{
+				state = Inc;
+			}
+			else if(GetBit(PINA, 1) && !GetBit(PINA, 0) && PORTB >= 0x00)
+			{
+				state = Stay2;
+			}
+			else if(GetBit(PINA, 0) && GetBit(PINA, 1))
+			{
+				state = Reset;
+			}
+			else
+			{
+				state = Stay2;
+			}
+        break;
+	
         case Reset:
-            if(GetBit(PINA, 0) && PORTB <= 0x09){//if incrementing
-                state = Inc;//increment
-            }
-            else if (GetBit(PINA, 1) && PORTB >= 0x00){
-                state = Dec; //decrement
-            }
-            else if (GetBit(PINA, 0) && GetBit(PINA, 1)){//both pressed together
-                state = Reset;
-            }
-			else if(!GetBit(PINA, 0) && !GetBit(PINA, 1)){ state = Init; }
+			if(GetBit(PINA, 0) && !GetBit(PINA, 1) && PORTB <= 0x09)
+			{
+ 				state = Inc;
+			}
+			else if(GetBit(PINA, 1) && !GetBit(PINA, 0) && PORTB >= 0x00)
+			{
+				state = Dec;
+			}
+			else if(GetBit(PINA, 0) && GetBit(PINA, 1))
+			{
+				state = Reset;
+			}
+			else
+			{
+				state = Reset;
+			}
         break;
 
 		default:
 		break;
 	}
-	
-	switch (state) { //State Actions
-		case Start:
-		PORTB = 0x07; break;
-				
+	//////////////////////////////////////////////////////
+	switch (state) { //State Actions		
 		case Init:
+			PORTB = 0x07;
 			break;
 
 		case Inc:
-            PORTB = Increment(PORTB); break;
+            PORTB = PORTB+1; 
+			break;
 		
 		case Dec:
-            PORTB = Decrement(PORTB); break;		
+            PORTB = PORTB-1; 
+			break;
 
         case Stay://do not change portB
-            PORTB = PORTB; break;
+			PORTB = PORTB;
+			break;
 
 		case Reset:
-		    PORTB = 0x00; break;
+		    PORTB = 0x00; 
+			break;
 
 		default:
 		break;	
@@ -138,7 +183,7 @@ int main(void)
 	DDRA = 0x00; PORTA = 0xFF; //inputs, 2 buttons
 	DDRB = 0xFF; PORTB = 0x00; //outputs 
 	
-	state = bla;//initialize state
+	state = Init;//initialize state
 	
 	while(1) { tick();}
 }
