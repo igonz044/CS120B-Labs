c1464d6bed177a1a1c4a49a951814fc46db5d7ce
Itzel <44009297+igonz044@users.noreply.github.com>
2019-04-19 09:37:54 -0700

    Update main.c

--- lab4/igonz044_lab4_challenge/igonz044_lab4_challenge/main.c
+++ lab4/igonz044_lab4_challenge/igonz044_lab4_challenge/main.c
@@ -10,12 +10,13 @@
  * code, is my own original work.
  */  
 #include <avr/io.h>
-enum States {Locked, Wait, Unlocked, Wait2} state;
+enum States {Locked, Wait, Unlocked, Wait2, Unlocked2} state;
 
 //Global variables here
 #define A0 (PINA & 0x01)
 #define A1 (PINA & 0x02)
 #define A2 (PINA & 0x04)
+#define A7 (PINA & 0x80)
 /*
 unsigned char SetBit(unsigned char x, unsigned char k, unsigned char b){
 	return (b ? x | (0x01 << k) : x & ~(0x01 << k));
@@ -59,9 +60,9 @@ void tick()
 		{
 			state = Locked;
 		}
-		else if()
+		else if(A2 && !A0 && !A1)
 		{
-			state = Wait2 
+			state = Wait2;
 		}
 		else
 		{
@@ -76,25 +77,45 @@ void tick()
 			}
 			else
 			{
-				state = Wait;
+				state = Wait2;
 			}
 		break;
+		
+		/*case Unlocked2://opens vault
+		if(A7)// to lock it back up
+		{
+			state = Locked;
+		}
+		else if(A2 && !A0 && !A1)
+		{
+			state = Wait2;
+		}
+		else
+		{
+			state = Unlocked2;
+		}
+		break;*/
 
 		default: 
 		break;
 	}	
 	switch (state) { //State Actions
-		case Unlocked:
-			PORTB = 0x01;
+		case Locked:
+			PORTB = 0x00;
 		break;
 		
 		case Wait:
+			PORTB = 0x00;
+		break;
+		
+		case Wait2:
+		PORTB = 0x01;
+		break;
+		
+		case Unlocked://opens vault
 			PORTB = 0x01;
 		break;
 		
-		case Locked://opens vault
-			PORTB = 0x00;
-		break;
 
 		default: 
 		break;
@@ -105,7 +126,7 @@ int main(void)
 	DDRA = 0x00; PORTA = 0xFF; //inputs, 2 buttons
 	DDRB = 0xFF; PORTB = 0x00; //outputs
 	
-	state = Unlocked;//initialize state
+	state = Locked;//initialize state
 	
 	while(1) { tick(); }
 	return 0;
